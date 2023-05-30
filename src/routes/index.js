b1af12e027b26ddf26c641c8be133cf0cab20176
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-05-30 16:06:12 -0500

    correction of routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userRouter = require("./USER.ROUTER.JS");
+const userRouter = require("./user.router");
 const router = express.Router();
 
 // colocar las rutas aquí
